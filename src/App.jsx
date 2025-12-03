4f5a18441c3bb56f1fed56d37277a6dda594ff87
zakaja <zakja3@gmail.com>
2025-12-03 12:49:19 +0100

    Navbar created

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navbar from './sections/Navbar'
+
+const App = () => {
+  return (
+  <div className='container mx-auto max-w-7xl'>
+    <Navbar/>
+    {/* hero */}
+    {/* about */}
+    {/* projects */}
+    {/* experience */}
+    {/* testimonial */}
+    {/* contact */}
+    {/* footer */}
+  </div>
+  )
+}
+
+export default App
